44202baf5ff1ab7d2ae3d2c33382b201f044f275
carmholm <ameliacarmenholmes@gmail.com>
2016-04-15 18:23:25 +0000

    Array Method Workshop - Exercise 3

--- answers.js
+++ answers.js
@@ -39,5 +39,25 @@ function getPositives(array){
     });
 };
 
-console.log(getPositives(nums));
+//console.log(getPositives(nums));
 
+//Exercise 3
+//Write a function called filterArray that takes an array AND a function as arguments. Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
+
+//NOTE: You are allowed to use Array.prototype.filter to answer this question.
+
+//NOTE 2: This is a bit of a trick question, the answer is a one-liner :)
+
+var things = [false, 0, "I am a string", 5];
+ 
+function func (thing){
+    if (thing){
+        return thing;
+    }
+};
+ 
+function filterArray (array, func){
+    return array.filter(func)
+};
+
+//console.log(filterArray(things, func));
