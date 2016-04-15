5ed66d07c98bdcd3eaad7a68d2092d6dbf6658d0
carmholm <ameliacarmenholmes@gmail.com>
2016-04-15 18:04:54 +0000

    Array Methods Workshop - Exercise 2

--- answers.js
+++ answers.js
@@ -15,4 +15,29 @@ function printPositives(array){
     return positiveNumbers;
 };
 
-console.log(printPositives(numbers));
+//console.log(printPositives(numbers));
+
+//Exercise 2
+//Write a function called getPositives that takes an array and uses its filter method to return a new array with only the positive numbers
+
+var nums = [2,-6,4,-9,5];
+
+function positive (number){
+    if (number > 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+function getPositives(array){
+   return array.filter(function(number){
+        if (positive(number)) {
+            return number;
+        } 
+    });
+};
+
+console.log(getPositives(nums));
+
